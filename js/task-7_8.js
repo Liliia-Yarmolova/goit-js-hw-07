ae63c6164bd66e41698fb988cd8c0bed9f9f8d8a
Liliia Yarmolova <lyarmolova@ukr.net>
2020-11-16 16:37:33 +0200

    Добавляю решение 8 задачи

--- js/task-7_8.js
+++ js/task-7_8.js
@@ -12,27 +12,35 @@
 Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
 Создай функцию destroyBoxes(), которая очищает div#boxes.*/
 
-// const inputRef = document.querySelector('#controls')
-// const btnRender = document.querySelector('button[data-action="render"]')
-// const btnDestroy = document.querySelector('button[data-action="destroy"]')
-// const boxesRef = document.querySelector('#boxes')
+const inputRef = document.querySelector('input[type="number"]')
+const btnRender = document.querySelector('button[data-action="render"]')
+const btnDestroy = document.querySelector('button[data-action="destroy"]')
+const boxesRef = document.querySelector('#boxes')
 
-// // inputRef.addEventListener('keypress', (e) =>  console.log(e))
-// // inputRef.addEventListener('keypress', countOfElements)
-// btnRender.addEventListener('click', renderElements)
+btnRender.addEventListener('click', renderElements)
+btnDestroy.addEventListener('click', destroyBoxes )
 
+function renderElements() {
+    const countOfElements = inputRef.value;
+    const divItemArr = []
+     for (let i = 1; i <= countOfElements; i++){
+        const divItem = document.createElement('div');
+        divItem.style.width = i * 10 + 20 + 'px';
+        divItem.style.height = i * 10 + 20 + 'px';
+        divItem.style.background = randomColor()
+         divItemArr.push(divItem)
+ }
+    boxesRef.append(...divItemArr)
+}
 
-// function renderElements(event) {
-//     let countOfElements = inputRef.value
-//     const divItemArr = []
-//     for (let i = 1; i <= countOfElements; i++){
-//         const divItem = document.createElement('div');
-//         divItem.style.width = (i*10 ) +20 +'px';
-//         divItem.style.height = (i*10 ) +20 +'px';
-//         divItem.textContent = i
-//         divItemArr.push(divItem)
-//     }
-//     boxesRef.append(...divItemArr)
-// }
+function destroyBoxes() {
+    boxesRef.textContent =''
+}
 
+function randomColor() {
+    let r = ('0'+(Math.random()*256|0).toString(16)).slice(-2),
+    g = ('0'+(Math.random()*256|0).toString(16)).slice(-2),
+    b = ('0'+(Math.random()*256|0).toString(16)).slice(-2);
+    return '#' +r+g+b;
 
+}
